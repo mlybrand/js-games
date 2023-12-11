b841899a83c75e2cc609094938ed222c469e1f54
Sebastian Lybrand <sebastian.lybrand@gmail.com>
2023-12-10 18:24:29 -0800

    committing

--- seblyb/exercise4/drawing.js
+++ seblyb/exercise4/drawing.js
@@ -27,8 +27,10 @@ function draw_grid(ctx, minor, major, stroke, fill) {
 }
 function draw_ship(ctx, radius, options) {
   options = options || {};
+  let angle = (options.angle || 0.5 * Math.PI) / 2;
+  let curve1 = options.curve1 || 0.25;
+  let curve2 = options.curve2 || 0.75;
   ctx.save();
-  // optionally draw a guide showing the collision radius
   if (options.guide) {
     ctx.strokeStyle = "white";
     ctx.fillStyle = "rgba(0, 0, 0, 0.25)";
@@ -38,24 +40,33 @@ function draw_ship(ctx, radius, options) {
     ctx.stroke();
     ctx.fill();
   }
-  // set some default values
   ctx.lineWidth = options.lineWidth || 2;
   ctx.strokeStyle = options.stroke || "white";
   ctx.fillStyle = options.fill || "black";
-  let angle = (options.angle || 0.5 * Math.PI) / 2;
-  // draw the ship in three lines
   ctx.beginPath();
   ctx.moveTo(radius, 0);
   ctx.lineTo(
     Math.cos(Math.PI - angle) * radius,
     Math.sin(Math.PI - angle) * radius
   );
-  ctx.lineTo(
+  ctx.quadraticCurveTo(radius * curve - radius, 0,
     Math.cos(Math.PI + angle) * radius,
     Math.sin(Math.PI + angle) * radius
   );
   ctx.closePath();
   ctx.fill();
   ctx.stroke();
+
+  if (options.guide) {
+    ctx.strokeStyle = "white";
+    ctx.lineWidth = 0.5;
+    ctx.beginPath();
+    ctx.moveTo(-radius, 0);
+    ctx.lineTo(0, 0);
+    ctx.stroke();
+    ctx.beginPath();
+    ctx.arc(radius * curve - radius, 0, radius / 50, 0, 2 * Math.PI);
+    ctx.stroke();
+  }
   ctx.restore();
 }
